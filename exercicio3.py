be337273a3640e56b847ca8d265ea5973733ec25
NicolasAskel <70597473+NicolasAskel@users.noreply.github.com>
2020-09-29 16:10:20 -0300

    Create exercicio3.py

---
+++ exercicio3.py
@@ -0,0 +1,6 @@
+palavra = input ("digite uma palavra pra ser escrita ao inverso:")
+
+def parametroinverso(palavra):
+   palavraInversa = palavra[::-1]
+   print(palavraInversa)
+parametroinverso(palavra)
